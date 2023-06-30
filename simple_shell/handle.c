1eed7db0a3874d8b5e0a0d3953ea320e6b96268d
Abdelmoneim000 <ab000arabi22@gmail.com>
2023-06-30 04:35:59 +0300

    hpath

--- simple_shell/handle.c
+++ simple_shell/handle.c
@@ -1,44 +1,44 @@
 #include "main.h"
-#include "string.h"
+#include <string.h>
 char *hpath(char **argv)
 {
-    char **token;
-    int check = 0;
-    char *path;
-    char *cmd_path = NULL;
-    int i;
-    path = my_getenv("PATH");
+    char *path = my_getenv("PATH");
     if (path == NULL)
     {
         fprintf(stderr, "Error: PATH environment variable not set\n");
         return NULL;
     }
-	token = split_line(path);
-    while (token)
+
+    char **directories = split_line(path);
+    free(path);
+
+    char *cmd_path = NULL;
+    int i = 0;
+    while (directories[i] != NULL)
     {
-        cmd_path = malloc(strlen(token[i]) + strlen(argv[0]) + 2);
+        cmd_path = malloc(strlen(directories[i]) + strlen(argv[0]) + 2);
         if (cmd_path == NULL)
         {
             perror("Error allocating memory");
-            free(path);
+            free_split(directories);
             return NULL;
         }
-        sprintf(cmd_path, "%s/%s", token[i], argv[0]);
+
+        sprintf(cmd_path, "%s/%s", directories[i], argv[0]);
+
         if (access(cmd_path, X_OK) == 0)
         {
-            check = 1;
-            break;
+            free_split(directories);
+            return cmd_path;
         }
-	i++;
+
         free(cmd_path);
         cmd_path = NULL;
+        i++;
     }
-    free(path);
-    
-    if (!check)
-    {
-        fprintf(stderr, "Error: Command '%s' not found in PATH\n", argv[0]);
-        return NULL;
-    }
-    return cmd_path;
+
+    free_split(directories);
+
+    fprintf(stderr, "Error: Command '%s' not found in PATH\n", argv[0]);
+    return NULL;
 }
