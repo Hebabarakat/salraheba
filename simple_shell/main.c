fb1708cb9ed4a485e6b0a798bf4f0d36437618e8
hebabarkat <heba_barakat2001@yahoo.com>
2023-07-09 08:59:04 -0700

    change

---
+++ simple_shell/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+int main()
+{
+char input[50];
+while (1)
+{
+printf ("$ ");
+scanf ("%s", input);
+//print prompt
+}
+return (0);
+}
---
+++ simple_shell/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#define DELMITER "\t\r\n\a"
+extern char **environ;
+#endif
