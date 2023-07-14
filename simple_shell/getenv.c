09ca11c19d607428ac6aade1f0e50d9129255203
Sarahgad <sarahgad91@gmail.com>
2023-07-14 11:43:51 -0700

    add getenv

---
+++ simple_shell/getenv.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+
+int my_strncmp(const char *s1, const char *s2, size_t n) {
+    size_t i;
+    for (i = 0; i < n && s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if (s1[i] != s2[i]) {
+            return s1[i] - s2[i];
+        }
+    }
+    if (i == n) {
+        return 0;
+    }
+    return s1[i] - s2[i];
+}
+
+char *_getenv(const char *name)
+{
+    int i;
+    char *token;
+
+    i = 0;
+    while(environ[i])
+    {
+    token = strtok(environ[i],"=");
+        if (strcmp(token, environ[i]) == 0)
+       	{
+            return(NULL,"\n");
+        }
+	i++;
+    }
+    return (NULL);
+}
+
+char *commandpath (char *cmd)
+{
+	char *path;
+	char *toknen;
+	char *cmd_full;
+	§
+}
