d86bd72652f0c7d82b5bfd6224a6a2661e1cf741
Sarahgad <sarahgad91@gmail.com>
2023-07-16 11:08:05 -0700

    noninteract-solve empty line

--- simple_shell/non_interactive.c
+++ simple_shell/non_interactive.c
@@ -10,31 +10,21 @@ void non_interactive (void)
 char *lineptr;
 size_t n;
 ssize_t nreads;
+char **token;
 n = 0;
 lineptr = NULL;
-nreads = getline(&lineptr,&n,stdin);
- read_noninteractiv(lineptr, nreads);
-}
-
-/**
- *
- *
- *
- */
-void read_noninteractiv(char *lineptr, ssize_t nreads)
+while ((nreads = getline(&lineptr,&n,stdin)) != -1)
 {
-size_t n;
-char **token;
-
-n = 0;
 if (nreads == -1)
 {
 perror ("nreads");
 exit(EXIT_FAILURE);
 }
-	while (nreads != -1)
-{
-         if (lineptr[strlen(lineptr) - 1] == '\n')
+	if(nreads == 1)
+	{
+		continue;
+	}	
+       	if (lineptr[strlen(lineptr) - 1] == '\n')
         {
             lineptr[strlen(lineptr) - 1] = '\0';
         }
@@ -54,7 +44,7 @@ exit(EXIT_FAILURE);
             exit(0);
     }
 _execve(token);
-free (lineptr);
-nreads = getline(&lineptr,&n,stdin);
+
 }
 }
+
